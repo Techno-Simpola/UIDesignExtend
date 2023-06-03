a939143a9071d056251b347eed23aff23a97ebef
Simpola <aksr.20u10488@btech.nitdgp.ac.in>
2023-06-03 06:32:49 +0530

    completed js and now adding more pages

--- script.js
+++ script.js
@@ -1,46 +1,36 @@
-gsap.to("#page1", {
-    scrollTrigger: {
-        trigger: "#page1",
-        start: "top top", //screen-top -then- element-top
-        end: "bottom top",
-        markers: true,
-        pin: true,
-        scroller: "#main"
-    }
-})
-
-gsap.to("#page1", {
-    scrollTrigger: {
-        trigger: "#page1",
-        start: "top top", //screen-top -then- element-top
-        end: "bottom top",
-        markers: true,
-        pin: true,
-        scroller: "#main"
-    }
-})
-
-gsap.to("#page1", {
-    scrollTrigger: {
-        trigger: "#page1",
-        start: "top top", //screen-top -then- element-top
-        end: "bottom top",
-        markers: true,
-        pin: true,
-        scroller: "#main"
-    }
-})
-
-gsap.to("#page1", {
-    scrollTrigger: {
-        trigger: "#page1",
-        start: "top top", //screen-top -then- element-top
-        end: "bottom top",
-        markers: true,
-        pin: true,
-        scroller: "#main"
-    }
-})
+function locomotive() {
+    gsap.registerPlugin(ScrollTrigger);
+  
+    const locoScroll = new LocomotiveScroll({
+      el: document.querySelector("#main"),
+      smooth: true ,
+    });
+    locoScroll.on("scroll", ScrollTrigger.update);
+  
+    ScrollTrigger.scrollerProxy("#main", {
+      scrollTop(value) {
+        return arguments.length
+          ? locoScroll.scrollTo(value, 0, 0)
+          : locoScroll.scroll.instance.scroll.y;
+      },
+  
+      getBoundingClientRect() {
+        return {
+          top: 0,
+          left: 0,
+          width: window.innerWidth,
+          height: window.innerHeight,
+        };
+      },
+  
+      pinType: document.querySelector("#main").style.transform
+        ? "transform"
+        : "fixed",
+    });
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+    ScrollTrigger.refresh();
+  }
+  locomotive();
 
 const canvas = document.querySelector("canvas");
 const context = canvas.getContext("2d");
@@ -418,13 +408,44 @@ function scaleIamge(img, ctx) {
         );
 }
 
-gsap.to("page>canvas", {
+ScrollTrigger.create({
+    trigger: "#page>canvas",
+    pin: true,
+    // markers:true,
+    scroller: `#main`,
+    start: `top top`,
+    end: `600% top`,
+  });
+
+gsap.to("#page1", {
     scrollTrigger: {
-        trigger: "#page>canvas",
+        trigger: "#page1",
+        start: "top top", //screen-top -then- element-top
+        end: "bottom top",
+        markers: true,
         pin: true,
-        //markers: true,
-        scroller: "#main",
-        start: "top top",
-        end: "600% top",
+        scroller: "#main"
+    }
+})
+
+gsap.to("#page2", {
+    scrollTrigger: {
+        trigger: "#page2",
+        start: "top top", //screen-top -then- element-top
+        end: "bottom top",
+        markers: true,
+        pin: true,
+        scroller: "#main"
+    }
+})
+
+gsap.to("#page3", {
+    scrollTrigger: {
+        trigger: "#page3",
+        start: "top top", //screen-top -then- element-top
+        end: "bottom top",
+        markers: true,
+        pin: true,
+        scroller: "#main"
     }
 })
