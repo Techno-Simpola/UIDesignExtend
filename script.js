8b6f16e433c559a039f0539093ef14ea05a4f2f1
Simpola <aksr.20u10488@btech.nitdgp.ac.in>
2023-06-03 05:41:11 +0530

    adding the javascript file and css begining

---
+++ script.js
@@ -0,0 +1,43 @@
+gsap.to("#page1", {
+    scrollTrigger: {
+        trigger: "#page1",
+        start: "top top", //screen-top -then- element-top
+        end: "bottom top",
+        markers: true,
+        pin: true,
+        scroller: "#main"
+    }
+})
+
+gsap.to("#page1", {
+    scrollTrigger: {
+        trigger: "#page1",
+        start: "top top", //screen-top -then- element-top
+        end: "bottom top",
+        markers: true,
+        pin: true,
+        scroller: "#main"
+    }
+})
+
+gsap.to("#page1", {
+    scrollTrigger: {
+        trigger: "#page1",
+        start: "top top", //screen-top -then- element-top
+        end: "bottom top",
+        markers: true,
+        pin: true,
+        scroller: "#main"
+    }
+})
+
+gsap.to("#page1", {
+    scrollTrigger: {
+        trigger: "#page1",
+        start: "top top", //screen-top -then- element-top
+        end: "bottom top",
+        markers: true,
+        pin: true,
+        scroller: "#main"
+    }
+})
